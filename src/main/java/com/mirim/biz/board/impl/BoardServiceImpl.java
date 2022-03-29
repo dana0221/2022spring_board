8d6f77e5360fb4838c0098b75194324803b27301
dana0221 <dana8478@naver.com>
2022-03-29 11:57:27 +0900

    AOP 이해하기(LogAdvice 클래스 대체)

--- src/main/java/com/mirim/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/mirim/biz/board/impl/BoardServiceImpl.java
@@ -2,6 +2,7 @@ package com.mirim.biz.board.impl;
 
 import com.mirim.biz.board.BoardService;
 import com.mirim.biz.board.BoardVO;
+import com.mirim.biz.common.Log4jAdvice;
 import com.mirim.biz.common.LogAdvice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,39 +13,39 @@ import java.util.List;
 public class BoardServiceImpl implements BoardService {
     @Autowired
     private BoardDAO boardDAO;
-    private LogAdvice log;
+    private Log4jAdvice log;
 
     public BoardServiceImpl() {
-        log = new LogAdvice();
+        log = new Log4jAdvice();
     }
 
     @Override
     public void insertBoard(BoardVO vo) {
-        log.printLog();
+        log.printLogging();
         boardDAO.insertBoard(vo);
     }
 
     @Override
     public void updateBoard(BoardVO vo) {
-        log.printLog();
+        log.printLogging();
         boardDAO.updateBoard(vo);
     }
 
     @Override
     public void deleteBoard(BoardVO vo) {
-        log.printLog();
+        log.printLogging();
         boardDAO.deleteBoard(vo);
     }
 
     @Override
     public BoardVO getBoard(BoardVO vo) {
-        log.printLog();
+        log.printLogging();
         return boardDAO.getBoard(vo);
     }
 
     @Override
     public List<BoardVO> getBoardList(BoardVO vo) {
-        log.printLog();
+        log.printLogging();
         return boardDAO.getBoardList(vo);
     }
 }
