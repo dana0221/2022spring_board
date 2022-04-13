2431c6a474ece9d5613ce196afacda6d86f65aa0
dana0221 <dana8478@naver.com>
2022-04-13 09:21:48 +0900

    AfterReturningAdvice JoinPoint 사용

--- src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,20 @@
 package com.mirim.biz.common;
 
+import com.mirim.biz.user.UserVO;
+import org.aspectj.lang.JoinPoint;
+
 public class AfterReturningAdvice {
-    public void AfterReturningAdvice(){
-        System.out.println("[사후처리] 결과 값 리턴 후 동작");
+    public void AfterReturningAdvice(JoinPoint jp, Object returningObj){
+        String method = jp.getSignature().getName();
+
+        if(returningObj instanceof UserVO){ // 객체 타입 판별
+            UserVO user = (UserVO) returningObj;    // 다운캐스팅
+
+            if(user.getRole().equals("Admin")){
+                System.out.println(user.getName() + " 로그인(Admin)");
+            }
+        }
+
+        System.out.println("[사후처리] " + method + "() 메서드 리턴 값:" + returningObj.toString());
     }
 }
