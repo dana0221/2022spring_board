091bd447e82d754d895fda5343cd49ddb3ccc4ac
dana0221 <dana8478@naver.com>
2022-04-26 10:57:24 +0900

    AfterThrowingAdvice 예외처리 분류

--- src/main/java/com/mirim/biz/common/AfterThrowingAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterThrowingAdvice.java
@@ -5,6 +5,15 @@ import org.aspectj.lang.JoinPoint;
 public class AfterThrowingAdvice {
     public void exceptionLog(JoinPoint jp, Exception exceptObj){
         String method = jp.getSignature().getName();
-        System.out.println("[에외처리]" + method + "() 메서드 수행 중 발생된 예외 메시지 : " + exceptObj.getMessage());
+        System.out.println("[에외처리]" + method + "() 메서드 수행 중 예외 발생 : ");
+
+        // 객체가 동일한지 확인
+        if(exceptObj instanceof IllegalArgumentException){
+            System.out.println("부적합한 값이 입력되었습니다.");
+        }else if(exceptObj instanceof NumberFormatException){
+            System.out.println("숫자 형식의 값이 아닙니다.");
+        }else if(exceptObj instanceof Exception){
+            System.out.println("에러 발생");
+        }
     }
 }
